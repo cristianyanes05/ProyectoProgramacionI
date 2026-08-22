bae7b45d5c32376ea9b1cf905477f2f2a6947f05
cristian <cris.yanes05@gmail.com>
2026-08-22 20:00:15 -0300

    Agrego archivo perfil_equipo.py

---
+++ codigo/perfil_equipo.py
@@ -0,0 +1,3 @@
+# Desarrollen perfil_equipo.py dentro de la carpeta código del repositorio. 
+# El programa deberá solicitar el nombre del equipo, comisión, nombre de cada integrante y rol inicial en el proyecto.
+# • 
